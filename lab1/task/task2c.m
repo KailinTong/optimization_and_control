f696036e0348e735933511e10030293f193509ad
Nicolai Schwartze <nicolai.schwartze@omicronenergy.com>
2021-12-09 22:38:20 +0100

    save result from task 2c

--- lab1/task/task2c.m
+++ lab1/task/task2c.m
@@ -15,15 +15,18 @@ l1.V =  (l1.m_w + m_s)*p_l1(2);
 l1.k_1 = (l1.m_w + m_s)*p_l1(3); 
 disp('L1  estimated parameters:')
 disp(l1)
+save('L1_const.mat', 'l1');
 
 l2.m_w = (l/p_l2(1) - 1)*m_s;
 l2.V =  (l2.m_w + m_s)*p_l2(2); 
 l2.k_1 = (l2.m_w + m_s)*p_l2(3); 
 disp('L2  estimated parameters:')
 disp(l2)
+save('L2_const.mat', 'l2');
 
-linf.m_w = (l/p_l2(1) - 1)*m_s; 
-linf.V =  (linf.m_w + m_s)*p_l2(2); 
-linf.k_1 = (linf.m_w + m_s)*p_l2(3); 
+linf.m_w = (l/p_linf(1) - 1)*m_s; 
+linf.V =  (linf.m_w + m_s)*p_linf(2); 
+linf.k_1 = (linf.m_w + m_s)*p_linf(3); 
 disp('L infinite  estimated parameters:')
 disp(linf)
+save('Linf_const.mat', 'linf');
